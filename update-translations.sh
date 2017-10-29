5d69d983665a82f5496bfc5c8423aa91393262ae
Ilya Portnov <portnov@iportnov.ru>
2017-10-29 21:27:16 +0500

    Update i18n.

--- update-translations.sh
+++ update-translations.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-stack exec -- hgettext -o po/batchd.pot -k __ -k __f -k __s -k __sf src/**/*.hs
+HS=$(find src/ batchd-core/src batchd-amazonka/src batchd-docker/src/ batchd-libvirt/src -name \*.hs)
+stack exec -- hgettext -o po/batchd.pot -k __ -k __f -k __s -k __sf $HS
 
 cd po/
 for PO in *.po
